0f4cf113f26939b10ad92aa46b27748e0bd566bc
Jenny <jhsu2@hawaii.edu>
2020-11-29 19:32:45 -1000

    Mock profile implemented

--- src/src/MockProfile.jsx
+++ src/src/MockProfile.jsx
@@ -3,14 +3,16 @@ import {
   Segment,
   Header,
   Dropdown,
-  Form,
+  Form, Modal, Button
 } from 'semantic-ui-react';
 import _ from 'lodash';
 import PropTypes from 'prop-types';
+import swal from 'sweetalert';
 import InternshipsFilters from './InternshipFilters';
 import { recommendation, dropdownCareerInterest } from './RecommendationScript';
 
 function MockProfile({ onChildClick, passedData, skillsVal, careerVal }) {
+  const [open, setOpen] = React.useState(true);
 
   const internships = new InternshipsFilters();
   const data = internships.mergeData();
@@ -60,7 +62,6 @@ function MockProfile({ onChildClick, passedData, skillsVal, careerVal }) {
     setFilters();
   };
 
-
   //
   // const getLocation = (event, { value }) => {
   //   locationChange = value;
@@ -95,44 +96,104 @@ function MockProfile({ onChildClick, passedData, skillsVal, careerVal }) {
     top: '6.5rem',
   };
 
+  const onSubmit = () => {
+    if (careerChange.length === 0 || skillChange.length === 0) {
+      swal({
+        title: 'Fields are required',
+        text: '',
+        icon: 'error',
+      });
+      return;
+    }
+    setOpen(false);
+  };
 
   return (
-      <Segment style={sticky}>
-        <div style={{ paddingTop: '2rem' }}>
-          <Header>
-            Mock Profile
-            <Header.Content>
-              Total results found: {internships.total(passedData)}
-            </Header.Content>
-          </Header>
-        </div>
-        <div style={{ paddingTop: '2rem' }}>
-          <Form>
-            <Form.Field
-                fluid multiple selection clearable
-                control={Dropdown}
-                options={internships.dropdownSkills()}
-                label={{ children: 'Skills' }}
-                placeholder='Skills'
-                search
-                onChange={getSkills}
-            />
-          </Form>
-        </div>
-        <div style={{ paddingTop: '2rem' }}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Field
-                fluid multiple selection clearable
-                control={Dropdown}
-                options={dropdownCareerInterest()}
-                label={{ children: 'Career Interest' }}
-                placeholder='Career Interest'
-                search
-                onChange={getCareerInterest}
-            />
-          </Form>
-        </div>
-      </Segment>
+      <Modal
+          onClose={() => setOpen(false)}
+          onOpen={() => setOpen(true)}
+          open={open}
+          closeOnDimmerClick={false}
+          trigger={<div/>}
+      >
+        <Modal.Header>Enter your Information</Modal.Header>
+        <Modal.Content>
+          <Modal.Description>
+            <Form>
+              <Form.Field
+                  required
+                  fluid multiple selection clearable
+                  control={Dropdown}
+                  options={internships.dropdownSkills()}
+                  label={{ children: 'Skills' }}
+                  placeholder='Skills'
+                  search
+                  onChange={getSkills}
+              />
+            </Form>
+            <div style={{ paddingTop: '2rem' }}>
+              <Form onSubmit={handleSubmit}>
+                <Form.Field
+                    required
+                    fluid multiple selection clearable
+                    control={Dropdown}
+                    options={dropdownCareerInterest()}
+                    label={{ children: 'Career Interest' }}
+                    placeholder='Career Interest'
+                    search
+                    onChange={getCareerInterest}
+                />
+              </Form>
+            </div>
+          </Modal.Description>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+              content="Submit"
+              labelPosition='right'
+              icon='checkmark'
+              onClick={() => onSubmit()}
+              positive
+
+          />
+        </Modal.Actions>
+      </Modal>
+      // <Segment style={sticky}>
+      //   <div style={{ paddingTop: '2rem' }}>
+      //     <Header>
+      //       Mock Profile
+      //       <Header.Content>
+      //         Total results found: {internships.total(passedData)}
+      //       </Header.Content>
+      //     </Header>
+      //   </div>
+      //   <div style={{ paddingTop: '2rem' }}>
+      //     <Form>
+      //       <Form.Field
+      //           fluid multiple selection clearable
+      //           control={Dropdown}
+      //           options={internships.dropdownSkills()}
+      //           label={{ children: 'Skills' }}
+      //           placeholder='Skills'
+      //           search
+      //           onChange={getSkills}
+      //       />
+      //     </Form>
+      //   </div>
+      //   <div style={{ paddingTop: '2rem' }}>
+      //     <Form onSubmit={handleSubmit}>
+      //       <Form.Field
+      //           fluid multiple selection clearable
+      //           control={Dropdown}
+      //           options={dropdownCareerInterest()}
+      //           label={{ children: 'Career Interest' }}
+      //           placeholder='Career Interest'
+      //           search
+      //           onChange={getCareerInterest}
+      //       />
+      //     </Form>
+      //   </div>
+      // </Segment>
   );
 }
 
