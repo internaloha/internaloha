031a357ce0620f227cbfc5baadd5ac821c5e4c69
ruizaj <60155925+ruizaj@users.noreply.github.com>
2021-02-07 23:01:43 -1000

    Added all needed scraped info, still need to implement write file, fix bugs

--- scraper/scrapers/nsf-reu.js
+++ scraper/scrapers/nsf-reu.js
@@ -7,6 +7,7 @@ async function main() {
   const browser = await puppeteer.launch({
     headless: false,
   });
+  log.enableAll();
   try {
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36');
@@ -17,22 +18,35 @@ async function main() {
     await page.click('a[onclick="showItemsPerPageForm(event, \'All\', \'?unitid=5049\')"]');
     try {
       await page.waitForSelector('td[data-label="Site Information: "] > div > a');
-      const position = [];
+      const position = await page.evaluate(() => {
+        const positions = document.querySelectorAll('td[data-label="Site Information: "] > div > a');
+        const posList = [...positions];
+        return posList.map(pos => pos.innerText);
+      });
+      log.info(position);
+      log.info(position.length);
       const urls = await page.evaluate(() => {
         const urlFromWeb = document.querySelectorAll('td[data-label="Site Information: "] > div > a');
         const urlList = [...urlFromWeb];
-        position.push(urlList);
         return urlList.map(url => url.href);
       });
-      console.log(position);
-      //console.log(urls);
+      log.info(urls);
+      log.info(urls.length);
+      const company = await page.evaluate(() => {
+        const companies = document.querySelectorAll('td[data-label="Site Information: "] > div > strong');
+        const compList = [...companies];
+        return compList.map(com => com.innerText);
+      });
+      log.info(company);
+      log.info(company.length);
       await page.waitForSelector('td[data-label="Site Location: "] > div');
       const location = await page.evaluate(() => {
         const locations = document.querySelectorAll('td[data-label="Site Location: "] > div');
         const locList = [...locations];
         return locList.map(loc => loc.innerText);
       });
-      //console.log(location);
+      log.info(location);
+      log.info(location.length);
       const city = [];
       const state = [];
       for (let i = 0; i < location.length; i++) {
@@ -40,12 +54,37 @@ async function main() {
         city.push(loc[0]);
         state.push(loc[1]);
       }
-
-    } catch (err1) {
-      log.error(err1.message);
+      log.info(city);
+      log.info(city.length);
+      log.info(state);
+      log.info(state.length);
+      const description = await page.evaluate(() => {
+        const skills = document.querySelectorAll('td[data-label="Additional Information: "] > div ');
+        const skillList = [...skills];
+        return skillList.map(list => list.innerText);
+      });
+      log.info(description);
+      log.info(description.length);
+      try {
+        const lastScraped = new Date();
+        for (let i = 0; i < urls.length; i++) {
+          data.push({
+            url: urls[i],
+            position: position[i],
+            company: company[i],
+            location: { city: city[i], state: state[i] },
+            lastScraped: lastScraped,
+            description: description,
+          });
+        }
+      } catch (err1) {
+        log.error(err1.message);
+      }
+    } catch (err2) {
+      log.error(err2.message);
     }
-  } catch (err2) {
-    log.error(err2.message);
+  } catch (err3) {
+    log.error(err3.message);
   }
 }
 
