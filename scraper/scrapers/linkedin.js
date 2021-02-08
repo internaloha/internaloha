74c99b113c3ad0c69222a5857bca1b647ae85a0c
paulinewu <peihanwu@hawaii.edu>
2021-02-07 20:07:56 -1000

    Revert back to before change the design.

--- scraper/scrapers/linkedin.js
+++ scraper/scrapers/linkedin.js
@@ -1,122 +1,162 @@
 import Logger from 'loglevel';
-import { fetchInfo, autoScroll, startBrowser, writeToJSON } from './scraperFunctions.js';
-
-async function getData(page) {
-  const results = [];
-  for (let i = 0; i < 5; i++) {
-    // get title, company, description, city, and state
-    results.push(fetchInfo(page, 'h1[itemprop="title"]', 'innerText'));
-    results.push(fetchInfo(page, 'div[class="arDetailCompany"]', 'innerText'));
-    results.push(fetchInfo(page, 'div[itemprop="description"]', 'innerHTML'));
-    results.push(fetchInfo(page, 'span[itemprop="addressLocality"]', 'innerText'));
-    results.push(fetchInfo(page, 'span[itemprop="addressRegion"]', 'innerText'));
-  }
-  return Promise.all(results);
-}
-
-async function setSearchFilter(page) {
-  try {
-    await page.waitForSelector('input[id="searchview"]');
-    await page.type('input[id="searchview"]', 'internship');
-    await page.keyboard.press('Enter');
-    await page.waitForSelector('button[id="locations-filter-acc"]');
-    await page.click('button[id="locations-filter-acc"]');
-    await page.waitForSelector('input[id="locations-filter-input"]');
-    await page.click('input[id="locations-filter-input"]');
-    // Separated 'United' and 'States' so that dropdown list comes out
-    await page.type('input[id="locations-filter-input"]', 'United');
-    await page.type('input[id="locations-filter-input"]', ' States');
-    // Delay prevents code from bypassing page changes
-    /** await delay(5000);
-    await page.waitForSelector('li[id="locations-filter-input-option-0"]');
-    await page.click('li[id="locations-filter-input-option-0"]');
-    await delay(5000);* */
-  } catch (err2) {
-    Logger.debug(err2.message);
-  }
-}
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import { fetchInfo, autoScroll } from './scraperFunctions.js';
 
 async function main() {
   const data = [];
-  let browser;
-  let page;
-  Logger.enableAll(); // this enables console logging. Will replace with CLI args later.
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
   try {
-    Logger.info('Executing script...');
-    [browser, page] = await startBrowser();
+    const page = await browser.newPage();
+    await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36');
     await page.goto('https://www.linkedin.com/jobs/search?keywords=Computer%2BScience&location=United%2BStates&geoId=103644278&trk=public_jobs_jobs-search-bar_search-submit&f_TP=1%2C2%2C3%2C4&f_E=1&f_JT=I&redirect=false&position=1&pageNum=0');
+    await page.waitForSelector('section.results__list');
+    Logger.info('Fetching jobs...');
     await autoScroll(page);
-    await setSearchFilter(page);
-    /** let loadMore = true;
-     let loadCount = 0;
-     let totalInternships = 0;
-     // Sometimes infinite scroll stops and switches to a "load more" button
-     while (loadMore === true && loadCount <= 15) {
+    let loadMore = true;
+    let loadCount = 0;
+    let totalInternships = 0;
+    // Sometimes infinite scroll stops and switches to a "load more" button
+    while (loadMore === true && loadCount <= 15) {
       try {
         await page.waitForTimeout(1000);
         await page.click('button[data-tracking-control-name="infinite-scroller_show-more"]');
-          loadCount++;
+        loadCount++;
       } catch (e2) {
-          loadMore = false;
-          Logger.debug('Finished loading...');
+        loadMore = false;
+        Logger.debug('Finished loading...');
       }
-    }* */
-    /** const elements = await page.$$('li[class="result-card job-result-card result-card--with-hover-state"]');
-     const times = await page.evaluate(
-     () => Array.from(
-     // eslint-disable-next-line no-undef
-     document.querySelectorAll('div.result-card__meta.job-result-card__meta time:last-child'),
-     a => a.textContent,
-     ),
-     ); * */
-    let nextPage = true;
-    while (nextPage === true) {
-      try {
-        await page.waitForTimeout(2000);
-        await page.waitForSelector('div[class="mux-search-results"]');
-        await page.click('a[id="loadMoreJobs"]');
-        Logger.info('Nagivating to next page....');
-      } catch (e2) {
-        Logger.debug('Finished loading all pages.');
-        nextPage = false;
-      }
-      await page.waitForNavigation;
-      const totalPage = await page.evaluate(() => document.querySelectorAll('ul[class="pagination"] li').length);
-      // for loop allows for multiple iterations of pages -- start at 2 because initial landing is page 1
-      for (let i = 2; i <= totalPage; i++) {
-        // Fetching all urls in page into a list
-        const urls = await page.evaluate(() => {
-          const urlFromWeb = document.querySelectorAll('h3 a');
-          const urlList = [...urlFromWeb];
-          return urlList.map(url => url.href);
-        });
-        // Iterate through all internship positions
-        try {
-          for (let j = 0; j < urls.length; j++) {
-            await page.goto(urls[j]);
-            const lastScraped = new Date();
-            const [position, company, description, city, state] = await getData(page);
-            // const date = new Date(posted).toISOString();
-            data.push({
-              url: urls[i],
-              position: position,
-              company: company.trim(),
-              location: { city: city, state: state },
-              lastScraped: lastScraped,
-              description: description,
-            });
-          }
-        } catch (err1) {
-          Logger.error(err1.message);
-        }
-      }
-      // write results to JSON file
-      await writeToJSON(data, 'linkedin');
     }
-  } catch (err1) {
-    Logger.debug('Our Error:', err1.message);
-    Logger.debug('\nData successfully written!');
+    const elements = await page.$$('li[class="result-card job-result-card result-card--with-hover-state"]');
+    const times = await page.evaluate(
+        () => Array.from(
+            // eslint-disable-next-line no-undef
+            document.querySelectorAll('div.result-card__meta.job-result-card__meta time:last-child'),
+            a => a.textContent,
+        ),
+    );
+
+    const urls = await page.evaluate(
+        () => Array.from(
+            // eslint-disable-next-line no-undef
+            document.querySelectorAll('a.result-card__full-card-link'),
+            a => a.href,
+        ),
+    );
+
+    Logger.info('Total Listings:', elements.length);
+    const skippedURLs = [];
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      const time = times[i];
+      // sometimes clicking it doesn't show the panel, try/catch to allow it to keep going
+      try {
+        await page.waitForSelector('div[class="details-pane__content details-pane__content--show"]');
+        const position = await fetchInfo(page, 'h2.topcard__title', 'innerText');
+        const company = await fetchInfo(page, 'a[data-tracking-control-name="public_jobs_topcard_org_name" ]', 'innerText');
+        const location = await fetchInfo(page, 'span[class="topcard__flavor topcard__flavor--bullet"]', 'innerText');
+        const description = await fetchInfo(page, 'div[class="show-more-less-html__markup show-more-less-html__markup--clamp-after-5"]', 'innerHTML');
+        const date = new Date();
+        let daysBack = 0;
+        const lastScraped = new Date();
+        if (time.includes('hours') || (time.includes('hour')) || (time.includes('minute'))
+            || (time.includes('minutes'))) {
+          daysBack = 0;
+        } else if ((time.includes('week')) || (time.includes('weeks'))) {
+          daysBack = time.match(/\d+/g) * 7;
+        } else {
+          daysBack = time.match(/\d+/g);
+        }
+        date.setDate(date.getDate() - daysBack);
+        const posted = date;
+        let state = '';
+        if (!location.match(/([^,]*)/g)[2]) {
+          state = 'United States';
+        } else {
+          state = location.match(/([^,]*)/g)[2].trim();
+        }
+        data.push({
+          position: position,
+          company: company,
+          location: {
+            city: location.match(/([^,]*)/g)[0],
+            state: state,
+          },
+          posted: posted,
+          url: urls[i],
+          lastScraped: lastScraped,
+          description: description,
+        });
+        Logger.info(position);
+        totalInternships++;
+      } catch (err5) {
+        Logger.trace(err5.message);
+        Logger.trace('Skipping! Did not load...');
+        skippedURLs.push(urls[i]);
+      }
+      await element.click();
+    }
+
+    Logger.info('--- Going back to scrape the ones previously skipped ---');
+    // scraping the ones we skipped
+    for (let i = 0; i < skippedURLs.length; i++) {
+      await page.goto(skippedURLs[i]);
+      await page.waitForSelector('section.core-rail');
+      const position = await fetchInfo(page, 'h1.topcard__title', 'innerText');
+      const company = await fetchInfo(page, 'a[data-tracking-control-name="public_jobs_topcard_org_name"]', 'innerText');
+      const location = await fetchInfo(page, 'span[class="topcard__flavor topcard__flavor--bullet"]', 'innerText');
+      const description = await fetchInfo(page, 'div[class="show-more-less-html__markup show-more-less-html__markup--clamp-after-5"]', 'innerHTML');
+      const time = await fetchInfo(page, 'span.topcard__flavor--metadata.posted-time-ago__text', 'innerText');
+      const skills = 'N/A';
+      const date = new Date();
+      let daysBack = 0;
+      const lastScraped = new Date();
+      if (time.includes('hours') || (time.includes('hour')) || (time.includes('minute'))
+          || (time.includes('minutes'))) {
+        daysBack = 0;
+      } else if ((time.includes('week')) || (time.includes('weeks'))) {
+        daysBack = time.match(/\d+/g) * 7;
+      } else {
+        daysBack = time.match(/\d+/g) * 30;
+      }
+      date.setDate(date.getDate() - daysBack);
+      const posted = date;
+      let state = '';
+      if (!location.match(/([^,]*)/g)[2]) {
+        state = 'United States';
+      } else {
+        state = location.match(/([^,]*)/g)[2].trim();
+      }
+      data.push({
+        position: position,
+        company: company.trim(),
+        location: {
+          city: location.match(/([^,]*)/g)[0].trim(),
+          state: state.trim(),
+        },
+        posted: posted,
+        url: skippedURLs[i],
+        skills: skills,
+        lastScraped: lastScraped,
+        description: description,
+      });
+      Logger.info(position);
+      totalInternships++;
+    }
+    Logger.info('Total internships scraped:', totalInternships);
+    Logger.info('Closing browser!');
+    await browser.close();
+  } catch (e) {
+    Logger.debug('Our Error:', e.message);
     await browser.close();
   }
+
+  // write results to JSON file
+  await fs.writeFile('./data/canonical/linkedin.canonical.data.json',
+      JSON.stringify(data, null, 4), 'utf-8', err => (err ? Logger.trace('\nData not written!', err) :
+          Logger.debug('\nData successfully written!')));
 }
+
 main();
