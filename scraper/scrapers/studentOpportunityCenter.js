cfba05825a51183fd2d652a7674039129a08718e
paulinewu <peihanwu@hawaii.edu>
2021-03-04 21:39:02 -1000

    Update studentOpportunityCenter.js

--- scraper/scrapers/studentOpportunityCenter.js
+++ scraper/scrapers/studentOpportunityCenter.js
@@ -3,6 +3,19 @@ import fs from 'fs';
 import Logger from 'loglevel';
 import { checkHeadlessOrNot, startBrowser } from './scraper-functions.js';
 
+const USERNAME_SELECTOR = '#login-email';
+const PASSWORD_SELECTOR = '#login-password';
+const CTA_SELECTOR = '#login-submit-button';
+const credentials = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+
+async function getData(page) {
+  const results = [];
+  for (let i = 0; i < 5; i++) {
+  
+  }
+  return Promise.all(results);
+}
+
 export async function main(headless) {
   let browser;
   let page;
@@ -12,6 +25,12 @@ export async function main(headless) {
     Logger.info('Executing script...');
     [browser, page] = await startBrowser(headless);
     await page.goto('https://app.studentopportunitycenter.com/auth/login');
+    await page.click(USERNAME_SELECTOR);
+    await page.keyboard.type(credentials.studentOpportunityCenter.user);
+    await page.click(PASSWORD_SELECTOR);
+    await page.keyboard.type(credentials.studentOpportunityCenter.password);
+    await page.click(CTA_SELECTOR);
+    await page.waitForNavigation();
   } catch (e) {
     Logger.trace('Our Error: ', e.message);
   }
