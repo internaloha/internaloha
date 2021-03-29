ffdb74e8f6b867ca05f9588611f8ec8877b614ac
ruizaj <60155925+ruizaj@users.noreply.github.com>
2021-03-29 10:03:27 -1000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8f7985a5c44449678bb73af7219add57.js"
+  "./precache-manifest.2794504d7ff248786613816d37784bc5.js"
 );
 
 self.addEventListener('message', (event) => {
