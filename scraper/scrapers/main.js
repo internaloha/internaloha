aa095a91fc9f6188ed599ddd745e32facbbf5a75
Jenny <56209781+jhsuP@users.noreply.github.com>
2021-03-31 12:00:27 -1000

    wip

--- scraper/scrapers/main.js
+++ scraper/scrapers/main.js
@@ -1,4 +1,5 @@
 import Logger from 'loglevel';
+import commandLineUsage from 'command-line-usage';
 import acm from './acm.js';
 import apple from './apple.js';
 import aexpress from './aexpress.js';
@@ -19,6 +20,74 @@ import glassdoor from './glassdoor.js';
 import nsf_reu from './nsf-reu.js';
 
 const myArgs = process.argv.slice(2);
+
+const optionDefinitions = [
+  {
+    name: 'unattended',
+    description: 'Runs all the unattended scrapers in production mode',
+    typeLabel: '',
+  },
+  {
+    name: 'dev',
+    description: 'Runs all the scrapers in development mode. Valid option: open (opens browsers), closed (doesn\'t' +
+      ' open' +
+      ' up browsers)',
+    type: String,
+    multiple: true,
+    defaultOption: true,
+    typeLabel: '{underline option}',
+  },
+  {
+    name: 'dev',
+    description: 'Runs [scraper] in development mode. Valid option: open (opens browsers), closed (doesn\'t open' +
+      ' up browsers)',
+    type: String,
+    multiple: true,
+    defaultOption: true,
+    typeLabel: '{underline scraper} {underline option}',
+  },
+];
+
+const sections = [
+  {
+    header: 'Error',
+    content: `An error was thrown when trying to run 'npm run scrapers ${process.argv.slice(2)}'. Please refer to the guide below to see the specifications.`,
+  },
+  {
+    header: 'Synopsis',
+    content: [
+      'Script Format: $ npm run scrapers [<name>|unattended] [dev|prod] [open|closed]',
+      'Default is "prod" and "closed". ',
+    ],
+  },
+  {
+    header: 'Options',
+    optionList: optionDefinitions,
+  },
+  {
+    header: 'Examples',
+    content: [
+      {
+        desc: '1. Running scraper in production.',
+        example: '$ npm run scrapers unattended',
+      },
+      {
+        desc: '2. Running a scraper individually (eg. linkedin) with the browser closed. ',
+        example: '$ npm run scrapers linkedin, $ npm run scrapers linkedin dev closed',
+      },
+      {
+        desc: '3. Running a scraper individually (eg. linkedin) with the browser open.',
+        example: '$ npm run scrapers linkedin dev open',
+      },
+    ],
+  },
+  {
+    content: 'Detailed Documentation: {underline' +
+      ' https://internaloha.github.io/documentation/docs/developers/scrapers/running}',
+  },
+];
+
+const usage = commandLineUsage(sections);
 
 /**
  *
@@ -68,22 +137,22 @@ async function getData(scraperName, headless = true) {
     glassdoor: glassdoor,
     nsf_reu: nsf_reu,
   };
-  await list[scraperName](headless);
+  try {
+    await list[scraperName](headless);
+  } catch (e) {
+    console.log(usage);
+    process.exit(0);
+  }
 }
 
 async function main() {
   process.setMaxListeners(0);
-  if (myArgs.length === 3) {
-    Logger.enableAll();
-    if (myArgs[2] && myArgs[2].toLowerCase() === 'open') {
-      await getData(myArgs[0], false);
-    } else if (myArgs[2] && myArgs[2].toLowerCase() === 'close') {
-      await getData(myArgs[0], true);
-    } else {
-      console.log('Invalid argument supplied, please use "dev open" or "dev close". For example, npm run scrapers' +
-        ' dev open.');
-      process.exit(0);
-    }
+
+// default is running in production (doesn't open browsers)
+  if (myArgs.length === 0 || myArgs[0] === 'prod' || myArgs[0] === 'unattended') {
+      Logger.setLevel('warn');
+      await getAllData(true);
+// npm run dev [open|close], default is it doesn't up browsers
   } else if (myArgs[0] === 'dev') {
     Logger.enableAll();
     if (myArgs[1] && myArgs[1].toLowerCase() === 'open') {
@@ -91,22 +160,61 @@ async function main() {
     } else if (myArgs[1] && myArgs[1].toLowerCase() === 'close') {
       await getAllData(true);
     } else {
-      console.log('Invalid argument supplied, please use "dev open", "dev close", or "production');
-      process.exit(0);
+      // default for npm run dev is it doesn't open browsers
+      await getAllData(true);
     }
-  } else if (myArgs[0] === 'production') {
-    Logger.setLevel('warn');
-    await getAllData(true);
+  } else if (myArgs[0] !== 'dev' && myArgs.length === 1) {
+    Logger.enableAll();
+    await getData(myArgs[0], true);
+// eg. npm run acm dev open (default is close)
+  } else if (myArgs.length === 3) {
+      Logger.enableAll();
+      if (myArgs[2] && myArgs[2].toLowerCase() === 'open') {
+        await getData(myArgs[0], false);
+      } else if (myArgs[2] && myArgs[2].toLowerCase() === 'close') {
+        await getData(myArgs[0], true);
+      } else {
+        await getData(myArgs[0], true);
+      }
   } else {
-    console.log('Invalid argument supplied, please use "dev open", "dev close", or "production".');
+    console.log(usage);
     process.exit(0);
   }
 
-  Logger.info('Finished scraping!\nNow parsing');
-  multi_parse();
-  Logger.info('Finished parsing!\nNow getting statistics');
-  statistics();
-  Logger.info('Completed.');
+  // if (myArgs.length === 3) {
+  //   Logger.enableAll();
+  //   if (myArgs[2] && myArgs[2].toLowerCase() === 'open') {
+  //     await getData(myArgs[0], false);
+  //   } else if (myArgs[2] && myArgs[2].toLowerCase() === 'close') {
+  //     await getData(myArgs[0], true);
+  //   } else {
+  //     console.log('Invalid argument supplied, please use "dev open" or "dev close". For example, npm run scrapers' +
+  //       ' dev open.');
+  //     process.exit(0);
+  //   }
+  // } else if (myArgs[0] === 'dev') {
+  //   Logger.enableAll();
+  //   if (myArgs[1] && myArgs[1].toLowerCase() === 'open') {
+  //     await getAllData(false);
+  //   } else if (myArgs[1] && myArgs[1].toLowerCase() === 'close') {
+  //     await getAllData(true);
+  //   } else {
+  //     console.log('Invalid argument supplied, please use "dev open", "dev close", or "production');
+  //     process.exit(0);
+  //   }
+  // } else if (myArgs[0] === 'production') {
+  //   Logger.setLevel('warn');
+  //   await getAllData(true);
+  // } else {
+  //   Logger.setLevel('warn');
+  //   await getAllData(true);
+  // }
+  //
+  // Logger.info('Finished scraping!\nNow parsing');
+  // multi_parse();
+  // Logger.info('Finished parsing!\nNow getting statistics');
+  // statistics();
+  // Logger.info('Completed.');
 }
 
 main();
