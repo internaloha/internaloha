587f0fcd0985fb162888bcc8e1f055acd54a9798
trey-sumida <tasumida@hawaii.edu>
2021-06-07 12:58:14 -1000

    Grabs title

--- scraper/scrapers/soc.js
+++ scraper/scrapers/soc.js
@@ -9,12 +9,25 @@ async function getData(page) {
   const results = [];
   for (let i = 0; i < 6; i++) {
     // get title, company, description, city, state, and zip
-    results.push(fetchInfo(page, 'h1[itemprop="title"]', 'innerText'));
-    results.push(fetchInfo(page, 'div[class="arDetailCompany"]', 'innerText'));
-    results.push(fetchInfo(page, 'div[itemprop="description"]', 'innerHTML'));
-    results.push(fetchInfo(page, 'span[itemprop="addressLocality"]', 'innerText'));
-    results.push(fetchInfo(page, 'span[itemprop="addressRegion"]', 'innerText'));
-    results.push(fetchInfo(page, 'span[itemprop="postalCode"]', 'innerText'));
+    results.push(fetchInfo(page, '.opportunity-heading', 'innerHTML'));
+    results.push('N/A');
+    results.push(fetchInfo(page, 'p', 'innerText'));
+    try {
+      const labels = await page.evaluate(() => {
+        const tags = document.querySelectorAll('div .pl-6');
+        const returnValues = [];
+        tags.forEach((tag) => {
+          returnValues.push(tag.innerHTML);
+        });
+        return returnValues;
+      });
+      console.log(labels[2]);
+      results.push(labels);
+    } catch (e) {
+      console.log(e);
+    }
+    results.push('N/A');
+    results.push('N/A');
   }
   return Promise.all(results);
 }
@@ -37,12 +50,19 @@ export async function main(headless) {
     await page.waitForNavigation({ timeout: 120000 });
 
     // Searching with keyword 'computer science'
+    /*
     await page.click('#mat-input-0');
     await page.keyboard.type('Computer Science Internship');
     await page.keyboard.press('Enter');
     await page.waitForNavigation();
+     */
+    await page.waitForTimeout(3000);
+    await page.goto('https://app.studentopportunitycenter.com/app/search/');
+    await page.waitForSelector('#mat-input-0');
+    await page.click('#mat-input-0');
+    await page.keyboard.type('Computer Science Internship');
+    await page.keyboard.press('Enter');
 
-    await page.reload();
     await page.waitForTimeout(3000);
     await page.click('.mat-select-value');
     await page.waitForTimeout(3000);
@@ -50,20 +70,38 @@ export async function main(headless) {
     await page.waitForTimeout(5000);
     const urls = await page.evaluate(() => {
       const urlFromWeb = document.querySelectorAll('.pb-12 a');
-      console.log(urlFromWeb);
       const urlList = [...urlFromWeb];
       return urlList.map(url => url.href);
     });
-    console.log(urls);
-    await page.waitForNavigation({ timeout: 120000 });
 
-    await writeToJSON(data, 'acm');
+    try {
+      for (let j = 0; j < urls.length; j++) {
+        await page.goto(urls[j]);
+        const lastScraped = new Date();
+        const [position, company, description, city, state, zip] = await getData(page);
+        data.push({
+          url: urls[j],
+          position: position,
+          company: company.trim(),
+          location: { city: city, state: state, zip: zip },
+          lastScraped: lastScraped,
+          description: description,
+        });
+        console.log(data[j]);
+      }
+    } catch (err1) {
+      Logger.error(scraperName, err1.message);
+    }
+
+    console.log(data);
+
+    // await writeToJSON(data, 'soc');
     await browser.close();
   } catch (err2) {
     Logger.error(scraperName, err2.message);
     await browser.close();
   }
-  Logger.error(`Elapsed time for acm: ${moment(startTime).fromNow(true)} | ${data.length} listings scraped `);
+  Logger.error(`Elapsed time for soc: ${moment(startTime).fromNow(true)} | ${data.length} listings scraped `);
 }
 
 export default main;
