69ccccfc1dfa4b898edd1e7893e5c0e53d6a1bf8
trey-sumida <tasumida@hawaii.edu>
2021-06-09 18:51:47 -1000

    80000 hours scraper created

--- scraper/scrapers/80000hours.js
+++ scraper/scrapers/80000hours.js
@@ -2,16 +2,16 @@ import Logger from 'loglevel';
 import moment from 'moment';
 import { fetchInfo, startBrowser, writeToJSON } from './scraper-functions.js';
 
-async function getData(page) {
+async function getData(page, id) {
   const results = [];
   for (let i = 0; i < 6; i++) {
     // get title, company, description, city, state, and zip
-    results.push(fetchInfo(page, 'h1[itemprop="title"]', 'innerText'));
-    results.push(fetchInfo(page, 'div[class="arDetailCompany"]', 'innerText'));
-    results.push(fetchInfo(page, 'div[itemprop="description"]', 'innerHTML'));
-    results.push(fetchInfo(page, 'span[itemprop="addressLocality"]', 'innerText'));
-    results.push(fetchInfo(page, 'span[itemprop="addressRegion"]', 'innerText'));
-    results.push(fetchInfo(page, 'span[itemprop="postalCode"]', 'innerText'));
+    results.push(fetchInfo(page, `#${id} h5[class="panel-vacancy__title"]`, 'innerText'));
+    results.push(fetchInfo(page, `#${id} div[class="panel-vacancy__summary"]`, 'innerText'));
+    results.push(fetchInfo(page, `#${id} div[class="panel-vacancy__job-description"] p`, 'innerText'));
+    results.push(fetchInfo(page, `#${id} div[class="panel-vacancy__location"]`, 'innerText'));
+    results.push('N/A');
+    results.push('N/A');
   }
   return Promise.all(results);
 }
@@ -29,13 +29,32 @@ export async function main(headless) {
     await page.goto('https://80000hours.org/job-board/ai-safety-policy/?role-type=internship');
 
     await page.waitForSelector('.panel');
-    const urls = await page.evaluate(() => {
+    let urls = await page.evaluate(() => {
       const urlFromWeb = document.querySelectorAll('.panel a');
       const urlList = [...urlFromWeb];
       return urlList.map(url => url.href);
     });
 
-    console.log(urls);
+    urls = urls.filter((url) => url.includes('https://80000hours.org/job-board/?role='));
+
+    try {
+      for (let j = 0; j < urls.length; j++) {
+        const divID = urls[j].replace('https://80000hours.org/job-board/?role=', '');
+        await page.goto(urls[j]);
+        const lastScraped = new Date();
+        const [position, company, description, city, state, zip] = await getData(page, divID);
+        data.push({
+          url: urls[j],
+          position: position,
+          company: company.trim(),
+          location: { city: city, state: state, zip: zip },
+          lastScraped: lastScraped,
+          description: description,
+        });
+      }
+    } catch (err1) {
+      Logger.error(scraperName, err1.message);
+    }
 
     await writeToJSON(data, '80000hours');
     await browser.close();
