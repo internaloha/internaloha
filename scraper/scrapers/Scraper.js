3d75495f65bab70df9eae05c010838ed77b952c1
Shun <47809066+saitoshi@users.noreply.github.com>
2021-09-13 17:00:05 +0900

    Update Scraper.js

--- scraper/scrapers/Scraper.js
+++ scraper/scrapers/Scraper.js
@@ -7,39 +7,45 @@ import pluginStealth from 'puppeteer-extra-plugin-stealth';
 import randomUserAgent from 'random-useragent';
 import { fetchInfo, autoScroll } from './scraper-functions.js';
 
-const credentials = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-
 export class Scraper {
-  /** Initialize the scraper state and provide configuration info.
-   *  constructor(name, url, credentials, minimumListings, listingFilePath, statisticsFilePath) {}
-   * */
+  /** Initialize the scraper state and provide configuration info. * */
+  constructor(name, url, credentials, minimumListings, listingFilePath, statisticsFilePath) {
+    this.name = name;
+    this.url = url;
+    this.credentials = credentials;
+    this.minimumListings = minimumListings;
+    this.listingFilePath = listingFilePath;
+    this.statisticsFilePath = statisticsFilePath;
+  }
+
   /**
    * Go to the site and perform any login necessary.
    * @throws Error if login fails or site cannot be found.
    */
-  /**
-   * login() {
-   *  // Navigate to login page
-   *  switch(name) {
-   *    case angellist:
-   *        await page.type('input[id="user_email"]', credentials.name.user);
-   *        await page.type('input[id="user_password"]', credentials.name.password);
-   *        await page.click('input[class="c-button c-button--blue s-vgPadLeft1_5 s-vgPadRight1_5"]');
-   *        break;
-   *    case 'SOC':
-   *        await page.goto('https://app.studentopportunitycenter.com/auth/login');
-            await page.click(USERNAME_SELECTOR);
-            await page.keyboard.type(credentials.studentOpportunityCenter.user);
-            await page.click(PASSWORD_SELECTOR);
-            await page.keyboard.type(credentials.studentOpportunityCenter.password);
-            await page.click(CTA_SELECTOR);
-            await page.waitForNavigation();
-   *        break;
-   *     default:
-   *        break;
-   *  }
-   * }
-  */
+
+  async login() {
+    const credentials = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
+    // Navigate to login page
+    switch (name) {
+      case angellist:
+        await page.type('input[id="user_email"]', credentials.name.user);
+        await page.type('input[id="user_password"]', credentials.name.password);
+        await page.click('input[class="c-button c-button--blue s-vgPadLeft1_5 s-vgPadRight1_5"]');
+        break;
+      case 'SOC':
+        await page.goto('https://app.studentopportunitycenter.com/auth/login');
+        await page.click(USERNAME_SELECTOR);
+        await page.keyboard.type(credentials.studentOpportunityCenter.user);
+        await page.click(PASSWORD_SELECTOR);
+        await page.keyboard.type(credentials.studentOpportunityCenter.password);
+        await page.click(CTA_SELECTOR);
+        await page.waitForNavigation();
+        break;
+      default:
+        break;
+    }
+  }
+
   /**
    * Search for internship listings.
    * This can yield either a set of URLs to pages with listings, or a single page with all the listings.
