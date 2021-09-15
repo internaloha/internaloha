8c13796f41c8b917dd2fefaf8b2bc12d93443bc1
Shun <47809066+saitoshi@users.noreply.github.com>
2021-09-16 03:55:34 +0900

    Update Scraper.js

--- scraper/scrapers/Scraper.js
+++ scraper/scrapers/Scraper.js
@@ -22,13 +22,11 @@ export class Scraper {
    * Go to the site and perform any login necessary.
    * @throws Error if login fails or site cannot be found.
    */
-
-  async login(page, name) {
+  async login(name, page) {
     const credentials = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
-    const userName = credentials.name.user;
-    const password = credentials.name.password;
-    Logger.error('Starting scraper angellist at', moment().format('LT'));
-
+    await page.type('input[id="user_email"]', credentials.angellist.user);
+    await page.type('input[id="user_password"]', credentials.angellist.password);
+    await page.click('input[class="c-button c-button--blue s-vgPadLeft1_5 s-vgPadRight1_5"]');
   }
 
   /**
@@ -36,39 +34,39 @@ export class Scraper {
    * This can yield either a set of URLs to pages with listings, or a single page with all the listings.
    * @throws Error if the search generates an error, or if it does not yield minimumListings.
    */
-  async search(name, url) {
-    const data = [];
-    const startTime = new Date();
-    puppeteer.use(pluginStealth());
-    const { browser, page } = await startBrowser();
-    await page.setViewport({ width: 1366, height: 768 });
-    const userAgent = randomUserAgent.getRandom();
-
-
+  search(page) {
+    const results = [];
   }
+
   /**
-   * search() {
-   *
-   * }
-   *
    * Sets an internal cursor to point to the next listing to be parsed.
    * @return false if there are no more listings to parse.
    * @throws Error if a problem occurred getting the next listing.
-   *
-   *nextListing() {
-   *
-   *}
-   *
-   * parseListing() {
-   *
-   * }
-   *
-   * writeListings() {
-   *
-   * }
-   *
-   * writeStatistics() {
-   *
-   * }
    */
+  nextListing() {}
+
+  /**
+   * Parses the current listing.
+   * Adds the parsed listing to an internal object.
+   * @throws Error if a problem occurred parsing this listing.
+   */
+  parseListing() {}
+
+  /**
+   * Writes the listings to the outputFilePath.
+   * @throws Error if a problem occurred writing the listings.
+   */
+  writeListings() {}
+
+  /**
+   * Appends a line to the statisticsFilePath with statistics about this run.
+   * The statistics file is in CSV format.
+   * Statistics include:
+   *   * Name of scraper
+   *   * Date and time of the run.
+   *   * Elapsed time for this run.
+   *   * Total number of listings found.
+   *   * Any errors thrown (including short description)
+   */
+  writeStatistics() {}
 }
