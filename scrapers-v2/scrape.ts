77cb75713117d8efd1e2d5cd60a244a70dc12ea6
CalianaFortin <70739998+CalianaFortin@users.noreply.github.com>
2021-10-25 02:20:24 -1000

    Added apple scraper suggestions

--- scrapers-v2/scrape.ts
+++ scrapers-v2/scrape.ts
@@ -4,7 +4,7 @@ import { AngelListScraper } from './scrapers/Scraper.angellist';
 import { NsfScraper } from './scrapers/Scraper.nsf';
 import { CiscoScraper } from './scrapers/Scraper.cisco';
 import { DISCIPLINES } from './disciplines';
-import { SimplyHiredScraper } from './scrapers/Scraper.simplyHired';
+// import { SimplyHiredScraper } from './scrapers/Scraper.simplyHired';
 import { Apple } from './scrapers/Scraper.apple';
 import { LinkedinScraper } from './scrapers/Scraper.linkedin';
 import { ZipRecruiterScraper } from './scrapers/Scraper.ziprecruiter';
@@ -26,7 +26,7 @@ const scrapers = {
   cisco: new CiscoScraper(),
   linkedin: new LinkedinScraper(),
   nsf: new NsfScraper(),
-  simplyhired: new SimplyHiredScraper(),
+  // simplyhired: new SimplyHiredScraper(),
   ziprecruiter: new ZipRecruiterScraper(),
 };
 
--- scrapers-v2/scrapers/Scraper.apple.ts
+++ scrapers-v2/scrapers/Scraper.apple.ts
@@ -19,82 +19,54 @@ export class Apple extends Scraper {
     await this.page.goto(this.url);
   }
 
-  delay(ms: number) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-  /**
-   * Get the values associated with the passed selector and associated field.
-   * Because we can't do closures with puppeteer, special arguments are needed to pass selector and field into page.evaluate().
-   * See: https://github.com/puppeteer/puppeteer/blob/main/docs/api.md#pageevaluatepagefunction-args
-   * Also: we have to create a returnVals variable and await it, then return it.
-   * It's worth it because we call this function five times in generateListings.
-   */
-  public async getValuesOLD(selector, field) {
-    const returnVals = await this.page.evaluate((selector, field) => {
-      const vals = [];
-      const nodes = document.querySelectorAll(selector);
-      nodes.forEach(node => vals.push(node[field]));
-      return vals;
-    }, selector, field);
-    return returnVals;
-  }
-
-  public async getValues(selector, field) {
-    return await this.page.$$eval(selector, (nodes, field) => nodes.map(node => node[field]), field);
-  }
-
   async generateListings() {
     await super.generateListings();
-    await this.page.goto('https://jobs.apple.com/en-us/search?sort=relevance');
-    await this.page.type('input[id="searchview"]', 'internship');
-    await this.page.keyboard.press('Enter');
-    await this.page.waitForSelector('button[id="locations-filter-acc"]');
-    await this.page.click('button[id="locations-filter-acc"]');
-    await this.page.waitForSelector('input[id="locations-filter-input"]');
-    await this.page.click('input[id="locations-filter-input"]');
+    let pageNum = 1;
+    const listingsTable= '#tblResultSet';
 
-    // Separated 'United' and 'States' so that dropdown list comes out
-    await this.page.type('input[id="locations-filter-input"]', 'United');
-    await this.page.type('input[id="locations-filter-input"]', ' States');
+    //Create an function called pageUrl which puts the pageNum into the URL
+    const pageUrl = (pageNum) =>
+      `https://jobs.apple.com/en-us/search?location=united-states-USA&sort=relevance&search=internship&page=${pageNum}`;
 
-    // Delay prevents code from bypassing page changes
-    await this.delay(5000);
-    await this.page.waitForSelector('li[id="locations-filter-input-option-0"]');
-    await this.page.click('li[id="locations-filter-input-option-0"]');
-    await this.delay(5000);
+    //We navigate to the first page
+    await this.page.goto('https://jobs.apple.com/en-us/search?location=united-states-USA&sort=relevance&search' +
+      '=internship&page=1');
+    await this.page.waitForTimeout(1000); //Delay helps to stop websites from blocking pupetteer
 
-    // here is where you traverse the site and populate your this.Listings field with the listings.
-    await this.page.waitForSelector('a[class="table--advanced-search__title"]');
-    let urls = await this.getValues('a[class="table--advanced-search__title"]', 'href');
+    //We get the number of urls on the current page
+    let urls = await super.getValues('a[class="table--advanced-search__title"]', 'href');
 
+    while (await super.selectorExists(listingsTable)) {
 
-    // Now generate listings. All arrays are (hopefully!) the same length.
-    for (let i = 0; i < urls.length; i++) {
-      // const location = { city: cities[i], state: states[i], country: '' };
-      await this.page.goto(urls[i]);
-      // Positions
-      const positions = await this.getValues('h1[itemprop="title"]', 'innerText');
-      // Descriptions
-      const descriptions = await this.getValues('div[id="jd-description"]', 'innerHTML');
-      // States
-      const states = await this.getValues('span[itemprop="addressRegion"]', 'innerText');
-      //Cities
-      const cities = await this.getValues('span[itemprop="addressLocality"]', 'innerText');
+      //This for loop evaluates each url on the page
+      for (let i = 0; i < urls.length; i++) {
 
-      const location = { city: cities, state: states, country: 'United States' };
+        await this.page.goto(urls[i]);
 
-      const listing = new Listing({
-        url: urls[i], position: positions[i], location, company: 'Apple', description:
-        descriptions
-      });
-      this.listings.addListing(listing);
+        const positions = await super.getValues('h1[itemprop="title"]', 'innerText');
+        const descriptions = await super.getValues('div[id="jd-description"]', 'innerText');
+        const states = await super.getValues('span[itemprop="addressRegion"]', 'innerText');
+        const cities = await super.getValues('span[itemprop="addressLocality"]', 'innerText');
+        const location = { city: cities, state: states, country: 'United States' };
+
+        const listing = new Listing({
+          url: urls[i], position: positions, location, company: 'Apple', description:
+            descriptions
+        });
+        this.listings.addListing(listing);
+
+      }
+      
+      // Go to the next url.
+      this.page.goto(pageUrl(++pageNum), {waitUntil: 'networkidle2'});
+      // this is supposed to help with website naviagtion by considering navigationn to be finished if there are 2 network
+      // connections for at least 500 ms
+      await this.page.waitForTimeout(3000); //Delay helps to stop websites from blocking pupetteer
     }
   }
 
   async processListings() {
     await super.processListings();
-    // here is where you do any additional processing on the raw data now available in the this.listings field.
   }
 
 }
