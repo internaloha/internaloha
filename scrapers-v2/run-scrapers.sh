a1d2683da43c6a2dce87f3bf13d55ce82d9152d2
Philip Johnson <johnson@hawaii.edu>
2021-10-25 16:51:05 -1000

    Update run scrapers.

--- scrapers-v2/run-scrapers.sh
+++ scrapers-v2/run-scrapers.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-npm run scrape -- -l info -cf true -s nsf -ml 100
-npm run scrape -- -l info -cf true -s simplyhired -ml 1000
+npm run scrape -- -cf true -s nsf -ml 100
+npm run scrape -- -cf true -s simplyhired -ml 1000
+npm run scrape -- -cf true -s apple -ml 40
 
 npm run statistics -- -cf true
