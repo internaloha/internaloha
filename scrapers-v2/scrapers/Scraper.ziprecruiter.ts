3d9c20c0b9442362027f0b5e5994ba94256c1b43
Shun <47809066+saitoshi@users.noreply.github.com>
2021-10-28 02:50:51 +0900

    Update Scraper.ziprecruiter.ts

--- scrapers-v2/scrapers/Scraper.ziprecruiter.ts
+++ scrapers-v2/scrapers/Scraper.ziprecruiter.ts
@@ -1,5 +1,7 @@
 // import { Listing } from './Listing';
 import { Scraper } from './Scraper';
+import _ from 'underscore';
+
 const prefix = require('loglevel-plugin-prefix');
 /**
  * Fetches the information from the page.
@@ -87,70 +89,23 @@ export class ZipRecruiterScraper extends Scraper {
     super.generateListings();
     await this.page.goto('https://www.ziprecruiter.com/candidate/search?search=computer+science+internship&location=United+States&days=30&radius=25');
     await this.page.waitForNavigation;
-    // grab all links
-    const elements = await this.page.evaluate(
-      () => Array.from(
-        // eslint-disable-next-line no-undef
-        document.querySelectorAll('.job_link.t_job_link'),
-        a => a.getAttribute('href'),
-      ),
-    );
-    this.log.debug(elements.length);
 
-    let urls = await this.page.evaluate(() => {
-      const vals = [];
-      const nodes = document.querySelectorAll('a[class="job_link t_job_link"]');
-      nodes.forEach(node => vals.push(node['href']));
-      return vals;
-    });
-    this.log.debug(`URLS: \n${urls}`);
-
-    for (let i = 0; i < elements; i++) {
-      const element = elements[i];
-      await this.page.goto(element, { waitUntil: 'domcontentloaded' });
-      await this.page.waitForSelector('.pc_message');
-      await this.page.click('.pc_message');
-      const date = new Date();
-      let daysBack = 0;
-
-      const positions = await super.getValues('.job_title', 'innerText');
-      this.log.debug(`Positions: \n${positions}`);
-
-      const companies = await super.getValues('.hiring_company_text.t_company_name', 'innerText');
-      this.log.debug(`Companies: \n${companies}`);
-
-      const descriptions = await super.getValues('td[data-label="Additional Information: "] > div ', 'innerText');
-      this.log.debug(`Descriptions: \n${descriptions}`);
-
-      const posted = await super.getValues('.job_more span[class="data"]', 'innerText');
-      this.log.debug(`Posted: \n${posted}`);
-
-      if (posted.includes('yesterday')) {
-        daysBack = 1;
-      } else {
-        daysBack = posted.match(/\d+/g);
-      }
-      date.setDate(date.getDate() - daysBack);
+    let loadMore = true;
+    if (!loadMore) {
+      loadMore = false;
+      this.log.info('--- All jobs are Listed, no "Load More" button --- ');
+    } else {
+      let elements = await this.page.evaluate(
+        () => Array.from(
+          // eslint-disable-next-line no-undef
+          document.querySelectorAll('.job_link.t_job_link'),
+          a => a.getAttribute('href'),
+        ),
+      );
+      elements = _.uniq(elements);
+      this.log.info(`Found ${elements.length} listings`);
+      
     }
-
-    const locations = await super.getValues( 'span[data-name="address"]', 'innerText');
-    this.log.debug(`Locations: \n${locations}`);
-
-    const cities = [];
-    const states = [];
-    for (let i = 0; i < locations.length; i++) {
-      const loc = locations[i].split(', ');
-      cities.push(loc[0]);
-      states.push(loc[1]);
-    }
-
-    // let urls = await this.page.evaluate(() => {
-    //   const vals = [];
-    //   const nodes = document.querySelectorAll('a[class="job_link t_job_link"]');
-    //   nodes.forEach(node => vals.push(node['href']));
-    //   return vals;
-    // });
-
   }
 
   async processListings() {
