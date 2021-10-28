f261741b1103a1ff46ff69ca8008c1f62011f9cf
Shun <47809066+saitoshi@users.noreply.github.com>
2021-10-29 01:56:48 +0900

    Update Scraper.indeed.ts

--- scrapers-v2/scrapers/Scraper.indeed.ts
+++ scrapers-v2/scrapers/Scraper.indeed.ts
@@ -4,7 +4,7 @@ const prefix = require('loglevel-plugin-prefix');
 
 export class IndeedScrapper extends Scraper {
   constructor() {
-    super({ name: 'indeed', url: 'https://www.indeed.com/jobs?q=computer%20science%20intern&vjk=9eff9928b08349d4' });
+    super({ name: 'indeed', url: 'https://www.indeed.com/jobs?q=computer%20science%20intern&jt=internship&sort=date&fromage=14&vjk=07a16fd583142467' });
   }
 
   async launch() {
@@ -21,11 +21,32 @@ export class IndeedScrapper extends Scraper {
 
   async generateListings() {
     await super.generateListings();
-    // here is where you traverse the site and populate your this.Listings field with the listings.
+    // variable to store the page count of the URL; for indeed the page count increments by 10
+    let pageNum = 0;
+
+    const listingsTable = '#tblResultSet';
+
+    // pageUrl returns an URL containing the specified page number.
+    const pageUrl = (pageNum) =>
+      `https://www.indeed.com/jobs?q=computer%20science%20intern&jt=internship&sort=date&fromage=14&start=${pageNum}&vjk=dbc0c52cd8cc4e91`;
+
+    //go to the first page of job listings for Indeed
+    await this.page.goto(pageUrl(pageNum), { waitUntil: 'networkidle0' });
+
+    while (await super.selectorExists(listingsTable)) {
+      // collect the URLs of the list of jobs
+      let urls = await super.getValues('a[class="icl-Button icl-Button--primary icl-Button--lg icl-Button--block"]', 'href');
+      this.log.info(`Processing page ${pageNum} with ${urls.length} listings.`);
+
+
+    }
+
+
+
+
   }
 
   async processListings() {
     await super.processListings();
   }
-
 }
